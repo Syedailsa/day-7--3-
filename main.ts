30442b43567c355a31b2d2b49dec83b12b39d8f3
Ilsa. <ilsasyeda@gmail.com>
2024-03-14 16:34:22 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+let house = {
+    room1: "my room",
+    room2: "parents room",
+    room3: "guest room",
+}
+//storing values as objects
+console.log(house.room1);
+console.log(house.room2);
+console.log(house.room3);
